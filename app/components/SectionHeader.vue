b2f427280ff7198fb69a7ea827be2a9075b97f4c
Jason Clark <riceboylerdj@gmail.com>
2026-01-25 14:19:04 -0800

    Update gradient for Sectionheader

--- app/components/SectionHeader.vue
+++ app/components/SectionHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="font-oswald text-4xl bg-linear-210 from-indigo-300 to-indigo-600 dark:from-indigo-900 dark:to-indigo-500 uppercase text-center p-3 rounded-xl"
+    class="font-oswald text-4xl bg-linear-210 from-indigo-100 to-indigo-500 dark:from-indigo-900 dark:to-indigo-500 uppercase text-center p-3 rounded-xl"
   >
     <slot />
   </div>
