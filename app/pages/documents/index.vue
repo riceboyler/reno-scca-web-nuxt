86bc094701ec264845a404abea83565f3bdd1f76
Jason Clark <riceboylerdj@gmail.com>
2026-02-10 08:12:22 -0800

    Create Rules Page

--- app/pages/documents/index.vue
+++ app/pages/documents/index.vue
@@ -1,7 +1,32 @@
+f
 <template>
-  <div/>
+  <SectionHeader>Rules</SectionHeader>
+  <div
+    v-for="pgm in rulePrograms"
+    :key="pgm"
+    class="my-8"
+  >
+    <SubHeader>
+      <div class="text-center">{{ pgm }}</div>
+    </SubHeader>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const { data } = useFetch("/api/rules");
+  const rulePrograms = ref<string[]>([]);
+
+  watch(
+    () => data.value,
+    (newData) => {
+      if (newData) {
+        rulePrograms.value = Array.from(
+          new Set(newData.map((rule: { Program: string }) => rule.Program))
+        );
+      }
+    },
+    { immediate: true }
+  );
+</script>
 
 <style lang="postcss"></style>
--- app/pages/documents/rules.vue
+++ app/pages/documents/rules.vue
@@ -1,7 +1,50 @@
+f
 <template>
-  <div/>
+  <UContainer>
+    <SectionHeader>Rules</SectionHeader>
+    <div
+      v-for="pgm in rulePrograms"
+      :key="pgm"
+      class="my-8"
+    >
+      <SubHeader>
+        <div class="text-center">{{ pgm }}</div>
+      </SubHeader>
+      <div
+        v-for="rule in rules?.filter((rule) => rule.Program === pgm)"
+        :key="rule.ID"
+        class="rules-list mb-2 mt-4"
+      >
+        <NuxtLink
+          :href="rule.Path"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="ml-4 flex flex-row items-center"
+        >
+          {{ rule.LinkText }}
+          <UIcon
+            name="bx:link-external"
+            class="inline-block ml-1"
+          />
+        </NuxtLink>
+      </div>
+    </div>
+  </UContainer>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const { data: rules } = useFetch("/api/rules");
+  const rulePrograms = ref<string[]>([]);
 
-<style lang="postcss"></style>
+  watch(
+    () => rules.value,
+    (newData) => {
+      if (newData) {
+        rulePrograms.value = Array.from(
+          new Set(newData.map((rule: { Program: string }) => rule.Program))
+        );
+      }
+    },
+    { immediate: true }
+  );
+</script>
