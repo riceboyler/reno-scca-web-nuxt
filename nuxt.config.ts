3e193207c80efda5d94cd31eb236267a2f63c116
Jason Clark <riceboylerdj@gmail.com>
2026-02-18 19:39:16 -0800

    Add basics for Studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,24 @@ export default defineNuxtConfig({
     "@nuxt/scripts",
     "@nuxt/ui",
     "@nuxtjs/device",
+    "nuxt-studio",
   ],
   css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
   compatibilityDate: "2024-04-03",
   fonts: {},
+  studio: {
+    repository: {
+      provider: 'github',
+      owner: 'riceboyler',
+      repo: 'reno-scca-web-nuxt',
+      branch: 'main'
+    }
+  },
+  nitro: {
+    prerender: {
+      routes: ['/'],
+      crawlLinks: true
+    }
+  }
 });
